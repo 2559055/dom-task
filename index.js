8e918b475d49a2640f21e56426756d3c788548e2
2559055 <mohamed.hussein@student.moringaschool.com>
2023-10-15 13:11:10 -0700

    my DOM task

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// Remove the 'main#main' element
+const mainElement = document.getElementById('main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+// Create 'newHeader' variable that points to the 'victory' element and update its text
+const newHeader = document.getElementById('vina');
+if (newHeader) {
+  newHeader.textContent = "zaki";
+}
